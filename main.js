f03c6f6686378f586fba54d306e4465153a53405
walk8243 <walk8243@gmail.com>
2018-09-06 21:00:54 +0900

    configureにデフォルト値を設定

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const log4js = require('log4js');
 
+const defaults = {
+  appenders: {
+    out: { type: 'stdout', layout: { type: 'pattern', pattern: '%[[%d{yyyy/MM/dd hh:mm:ss.SSS}] [%p]%] - ' } },
+    err: { type: 'stderr', layout: { type: 'pattern', pattern: '%[[%d{yyyy/MM/dd hh:mm:ss.SSS}] [%p]%] - ' } },
+    logOut: { type: 'logLevelFilter', appender: 'out', level: 'TRACE', maxLevel: 'INFO' },
+    logErr: { type: 'logLevelFilter', appender: 'err', level: 'WARN' },
+  },
+  categories: {
+    default: { appenders: ['logOut', 'logErr'], level: 'ALL' },
+  },
+};
+
 module.exports = logger = () => {
-  return log4js.getLogger();
+  return log4js.configure(defaults).getLogger();
 };
--- test/main.js
+++ test/main.js
@@ -7,4 +7,14 @@ describe('Module main', () => {
     assert.equal(typeof main, 'function');
     assert.ok(main() instanceof require('log4js/lib/logger'));
   });
+
+  it('log check', () => {
+    var logger = main();
+    logger.trace('Entering cheese testing');
+    logger.debug('Got cheese.');
+    logger.info('Cheese is Comté.');
+    logger.warn('Cheese is quite smelly.');
+    logger.error('Cheese is too ripe!');
+    logger.fatal('Cheese was breeding ground for listeria.');
+  });
 });
