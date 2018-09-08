c5e8349a6a915bbf8c0c9c856b161c5acd96d222
walk8243 <walk8243@gmail.com>
2018-09-08 18:12:06 +0900

    ログの表示スタイルの複数定義してライブラリ化

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const main  = require('./main');
+const walk8243Logger  = require('./lib/logger');
 
-const logger = main();
-logger.info(logger);
-console.log();
+for(let key of ['getLogger', 'def', 'none', 'color', 'nocolor']) {
+  if(walk8243Logger.hasOwnProperty(key)) {
+    let logger = walk8243Logger[key]();
+    console.log(`\n- ${key}`);
+    console.log(logger);
+    showLogs(logger);
+  }
+}
+
+let logger = walk8243Logger.def('production');
+console.log(`\n- def`);
+console.log(logger);
 showLogs(logger);
+
 
 function showLogs(logger) {
   logger.trace('Entering walk8243 testing');
@@ -11,5 +21,5 @@ function showLogs(logger) {
   logger.info('Walk8243 commits.');
   logger.warn('Walk8243 looked sleepy.');
   logger.error('Walk8243 is drowsy!');
-  logger.fatal('Walk8243 fell asleep!');
+  logger.fatal('Walk8243 has fallen asleep!');
 }
